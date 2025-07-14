62614d1ae102b605cf6e1dec5414bfcf2eef4bd5
ddolly518 <20211109@sungshin.ac.kr>
2025-07-14 10:19:08 +0900

    feat: add initial Spring Boot project

---
+++ src/test/java/com/lectory/lectory/LectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lectory.lectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
