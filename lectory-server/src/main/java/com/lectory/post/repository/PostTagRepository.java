a08f99ac8f8ab77602d9f2a66ccf1e6077d30799
minchae624 <minchae0624@gmail.com>
2025-07-15 20:59:44 +0900

    feat: PostTagRepository 수정

--- lectory-server/src/main/java/com/lectory/post/repository/PostTagRepository.java
+++ lectory-server/src/main/java/com/lectory/post/repository/PostTagRepository.java
@@ -1,12 +1,16 @@
 package com.lectory.post.repository;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.lectory.common.domain.post.PostTag;
 import com.lectory.common.domain.post.PostTagId;
 
 public interface PostTagRepository extends JpaRepository<PostTag, PostTagId> {
-    Optional<PostTag> findByName(String name);
+    
+	@Query("select pt from PostTag pt where pt.tag.name = :name")
+    List<PostTag> findByTagName(@Param("name") String name);
 }
