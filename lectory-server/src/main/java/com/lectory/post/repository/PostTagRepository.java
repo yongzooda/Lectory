4e5398de0287e017bbf6782a636abb637ab9b555
minchae624 <minchae0624@gmail.com>
2025-07-16 10:33:33 +0900

    post 수정

--- lectory-server/src/main/java/com/lectory/post/repository/PostTagRepository.java
+++ lectory-server/src/main/java/com/lectory/post/repository/PostTagRepository.java
@@ -1,12 +1,9 @@
 package com.lectory.post.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lectory.common.domain.post.PostTag;
 import com.lectory.common.domain.post.PostTagId;
 
 public interface PostTagRepository extends JpaRepository<PostTag, PostTagId> {
-    Optional<PostTag> findByName(String name);
 }
