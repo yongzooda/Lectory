f4bcb1b4fd51467f1540ccdf8e553b4f459a8937
aogoggog <aogoggog@gmail.com>
2025-07-16 14:12:22 +0900

    Modify Directory Name Upper to Lower

---
+++ lectory-server/src/main/java/com/lectory/pay/service/RegularPayService.java
@@ -0,0 +1,32 @@
+package com.lectory.pay.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lectory.common.domain.pay.RegularPay;
+import com.lectory.pay.repository.RegularPayRepository;
+
+@Service
+public class RegularPayService {
+    @Autowired
+    private RegularPayRepository regularPayRepository;
+
+    public List<RegularPay> getAllRegularPays() {
+        return regularPayRepository.findAll();
+    }
+
+    public void deleteByUser_UserId(Long userId) {
+        regularPayRepository.deleteByUser_UserId(userId);
+    }
+
+    public void save(RegularPay regularPay) {
+        regularPayRepository.save(regularPay);
+    }
+
+    public RegularPay findByUser_UserId(Long userId) {
+        return regularPayRepository.findByUser_UserId(userId).orElse(null);
+    }
+
+}
