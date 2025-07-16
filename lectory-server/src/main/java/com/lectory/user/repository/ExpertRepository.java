253b69d3c44fb0616beb3497abbe8f9190f502ce
sinjaehyeon1 <sinjaehyeon1@naver.com>
2025-07-16 15:05:47 +0900

    feat: 전문가 마이페이지 API 로직 구현

--- lectory-server/src/main/java/com/lectory/user/repository/ExpertRepository.java
+++ lectory-server/src/main/java/com/lectory/user/repository/ExpertRepository.java
@@ -1,9 +1,13 @@
 package com.lectory.user.repository;
 
 import com.lectory.common.domain.user.Expert;
+import com.lectory.common.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ExpertRepository extends JpaRepository<Expert, Long> {
+    Optional<Expert> findByUser(User user);
 }
