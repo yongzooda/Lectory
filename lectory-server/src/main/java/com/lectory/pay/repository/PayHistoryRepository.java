2f7877928b1d88938e2f8472b65271a4bc2b9f49
aogoggog <aogoggog@gmail.com>
2025-07-16 14:27:38 +0900

    Modify Directory

--- lectory-server/src/main/java/com/lectory/pay/repository/PayHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lectory.pay.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lectory.common.domain.pay.PayHistory;
-
-public interface PayHistoryRepository extends JpaRepository<PayHistory, Long> {
-
-}
