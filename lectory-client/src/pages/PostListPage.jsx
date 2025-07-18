117feb037276a773c2239547147db7859b0f328d
minchae624 <minchae0624@gmail.com>
2025-07-18 13:53:40 +0900

    ㅔdto 수정

--- lectory-client/src/pages/PostListPage.jsx
+++ lectory-client/src/pages/PostListPage.jsx
@@ -13,7 +13,7 @@ export default function PostListPage({ userRole, userId }) {
   useEffect(() => {
     async function fetchPosts() {
       try {
-        const res = await axios.get("/post");
+        const res = await axios.get("/posts");
         console.log("🔍 raw posts response:", res.data);
 
         const raw = res.data;
