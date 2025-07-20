470f05401dd91f9761e7056576173b9b73eca8ac
yongju <yungju817@naver.com>
2025-07-21 03:49:24 +0900

    오류 수정

--- lectory-server/src/main/java/com/lectory/files/FileServeController.java
+++ lectory-server/src/main/java/com/lectory/files/FileServeController.java
@@ -13,7 +13,7 @@ public class FileServeController {
 
     private final FileStorageService storage;
 
-    @GetMapping("api/files/{id}")
+    @GetMapping("/api/files/{id}")
     public ResponseEntity<?> serve(@PathVariable String id,
                                    @RequestHeader(value = "Range", required = false) String range)
             throws Exception {
