1fd03611076bdf2aad93d8d093355ae53f984d65
aogoggog <aogoggog@gmail.com>
2025-07-22 17:48:07 +0900

    Modify Home URL In PaySuccess Page

--- lectory-server/src/main/java/com/lectory/post/dto/LikeRequestDto.java
+++ lectory-server/src/main/java/com/lectory/post/dto/LikeRequestDto.java
@@ -2,10 +2,13 @@ package com.lectory.post.dto;
 
 import com.lectory.common.domain.LikeTarget;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter
+@Setter
+@AllArgsConstructor
 public class LikeRequestDto {
     private LikeTarget target;
     private Long targetId;
