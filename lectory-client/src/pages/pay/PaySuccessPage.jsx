a2db07c1a9365f4574b569e59251ab6746213600
ddolly518 <20211109@sungshin.ac.kr>
2025-07-22 09:53:18 +0900

    style: pay footer 제거

--- lectory-client/src/pages/pay/PaySuccessPage.jsx
+++ lectory-client/src/pages/pay/PaySuccessPage.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import { Footer } from "../sections/Footer";
-import { NavbarLightDefault } from "../sections/NavbarLightDefault";
 import { PaySuccess } from "../sections/Pay";
 import "../../assets/css/page.css";
 
 export const PayPageSuccess = () => {
   return (
     <div className="page" data-model-id="2823:13576">
-      <NavbarLightDefault />
-      <Footer />
       <PaySuccess />
     </div>
   );
