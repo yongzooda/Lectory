069d660426f6b0f70184aaaa031fc970e793047e
gusgo200 <gusgo200@naver.com>
2025-07-22 09:15:49 +0900

    관리자-게시글 프론트 수정

--- lectory-client/src/api/jwtUtils.js
+++ lectory-client/src/api/jwtUtils.js
@@ -11,6 +11,16 @@ class JwtUtils {
       return null;
     }
   }
-}
 
+  static getRole(token) {
+    if (!token) return null;
+    try {
+      const decoded = jwtDecode(token);
+      return decoded.auth;
+    } catch (e) {
+      console.error("JWT 디코딩 오류 (role):", e);
+      return null;
+    }
+  }
+}
 export default JwtUtils;
